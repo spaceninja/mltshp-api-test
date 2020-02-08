cd9d2f3af029606047ba8f39318c0e16c05a179f
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-08 13:17:46 -0800

    add lambda function

---
+++ src/lambda/mltshp-oauth-api-proxy.js
@@ -0,0 +1,64 @@
+/**
+ * Get MLTSHP API Token Proxy
+ * Requests an access token from the MLTSHP OAuth API.
+ *
+ * @see https://mltshp.com/developers
+ */
+import fetch from "node-fetch";
+import querystring from "querystring";
+const mltshpOAuthApiUrl = "https://mltshp.com/api/token";
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  // Params are in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+  console.log("PARAMS", params);
+
+  // Reject if we don't have the expected parameters
+  if (
+    !(
+      params &&
+      params.code &&
+      params.client_id &&
+      params.client_secret &&
+      params.redirect_uri
+    )
+  ) {
+    return { statusCode: 401, body: "API Keys Missing" };
+  }
+
+  // Construct the body of the request
+  const body = {
+    grant_type: "authorization_code",
+    code: params.code,
+    client_id: params.client_id,
+    client_secret: params.client_secret,
+    redirect_uri: params.redirect_uri
+  };
+  const jsonBody = JSON.stringify(body);
+
+  // Request an access token from the oAuth API
+  return fetch(mltshpOAuthApiUrl, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: jsonBody
+  })
+    .then(response => response.json())
+    .then(json => ({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(json)
+    }))
+    .catch(error => ({
+      statusCode: 422,
+      body: `Oops! Something went wrong. ${error}`
+    }));
+};
