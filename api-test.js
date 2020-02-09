2513b5cc9d4a92180808d9f0742bb553e78371ed
Scott Vandehey <fake-scott@spaceninja.com>
2020-02-09 12:32:15 -0800

    code cleanup

--- api-test.js
+++ api-test.js
@@ -11,36 +11,36 @@ import base64 from "crypto-js/enc-base64";
  *
  * Start by setting up an app to get an API key & secret at
  * https://mltshp.com/developers
- *
- * TODO: convert to simple Node app
- *  x load secrets from environment vars
- *  x provide a simple server
- *  x update default redirect URL and instructions to use it
- *  - break app into three routes - index (logged out), login, image
- *  - ditch proxy functions if node app can connect to API
- *  - update dev docs with how to construct signature
- *  - update dev docs to link to repo
  */
 
 // Your app's API key, secret, and redirect URL
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 const REDIRECT_URL = process.env.REDIRECT_URL;
-console.log("ENV VARS", API_KEY, API_SECRET, REDIRECT_URL);
+console.table({
+  "API key": API_KEY,
+  "API secret": API_SECRET,
+  "Redirect URL": REDIRECT_URL
+});
 
 // Other API URLs
-const AUTHENTICATION_URL = `https://mltshp.com/api/authorize?response_type=code&client_id=${API_KEY}`;
-const ACCESS_TOKEN_URL = "https://mltshp.com/api/token";
-// const ACCESS_TOKEN_URL =
-//   "https://mltshp-api-test.netlify.com/.netlify/functions/mltshp-oauth-api-proxy";
+// Note, you'll need a proxy to access the API from the front-end
+// @see https://stackoverflow.com/a/43268098
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 const RESOURCE_URL = "https://mltshp.com";
+const ACCESS_TOKEN_URL = `${RESOURCE_URL}/api/token`;
+const AUTHENTICATION_URL = `${RESOURCE_URL}/api/authorize?response_type=code&client_id=${API_KEY}`;
 
-// Get the authorization code from the URL parameter
+// Check the URL parameters for an user authorization code from MTLSHP
 const urlParams = new URLSearchParams(window.location.search);
 const authCode = urlParams.get("code");
 
 // Prepare the document for content
 const appElement = document.getElementById("app");
+const loggedOutMessage = `
+  You need to authenticate this application with MLTSHP first.
+  <a href="${AUTHENTICATION_URL}">Go authenticate</a>.
+  (Be sure to click "I AGREE.")`;
 
 /**
  * Convert Decimal to Hex
@@ -50,9 +50,9 @@ const appElement = document.getElementById("app");
  * @returns {string}
  * @see https://stackoverflow.com/a/27747377
  */
-function dec2hex(dec) {
+const dec2hex = dec => {
   return ("0" + dec.toString(16)).substr(-2);
-}
+};
 
 /**
  * Generate a Nonce
@@ -62,18 +62,32 @@ function dec2hex(dec) {
  * @returns {string}
  * @see https://stackoverflow.com/a/27747377
  */
-function generateNonce(length = 40) {
+const generateNonce = (length = 40) => {
   var array = new Uint8Array(length / 2);
   window.crypto.getRandomValues(array);
   return Array.from(array, dec2hex).join("");
-}
+};
 
 /**
- * Generate API Request Signature
- * We must create a signed message for the server using the following:
+ * Handle Fetch Request Errors
+ *
+ * @param {object} response
+ * @returns {object}
+ * @see https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+ */
+const handleErrors = response => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
+/**
+ * Generate API Authorization String
+ * We must create a signed message for the API using the following:
  *  * The access token you just received.
- *  * A UTC timestamp
- *  * A nonce: a random string between 10 and 35 characters long.
+ *  * A UTC timestamp (in seconds)
+ *  * A nonce (random string between 10 and 35 characters long)
  *    Note: Don't use the UTC timestamp as a nonce!
  *  * Your request method (GET/POST)
  *  * The host (mltshp.com)
@@ -91,7 +105,7 @@ function generateNonce(length = 40) {
  * @param {string} method=GET
  * @returns {string}
  */
-const generateSignature = (token, path, method = "GET") => {
+const generateAuthString = (token, path, method = "GET") => {
   const timestamp = Math.floor(Date.now() / 1000);
   const nonce = generateNonce(35);
 
@@ -100,7 +114,9 @@ const generateSignature = (token, path, method = "GET") => {
   // You should use https for all API queries
   const port = 80;
 
-  // Normalize the message. The order and linebreaks are important!
+  // Normalize the message.
+  // NOTE: we're not including a query string because this demo doesn't need one.
+  // NOTE: The order, indentation, and linebreaks are important!
   const normalizedString = `${token.access_token}
 ${timestamp}
 ${nonce}
@@ -109,12 +125,22 @@ mltshp.com
 ${port}
 ${path}
 `;
-  console.log("NORMALIZED STRING", normalizedString);
 
+  // Create a signature by taking the normalizedString and use the secret to
+  // construct a has using SHA1 encoding, then Base64 the result.
   const hash = hmacSHA1(normalizedString, token.secret);
   const signature = base64.stringify(hash);
-  const authString = `MAC token=${token.access_token}, timestamp=${timestamp}, nonce=${nonce}, signature=${signature}`;
-  console.log("SIGNATURE", authString);
+
+  const authString =
+    `MAC token=${token.access_token}, ` +
+    `timestamp=${timestamp}, ` +
+    `nonce=${nonce}, ` +
+    `signature=${signature}`;
+
+  console.group("GENERATE SIGNATURE");
+  console.log(normalizedString);
+  console.log(authString);
+  console.groupEnd();
 
   return authString;
 };
@@ -129,21 +155,18 @@ ${path}
  * @returns {object}
  */
 const getResource = (token, path) => {
-  const authString = generateSignature(token, path);
+  const authString = generateAuthString(token, path);
   const endpoint = `${RESOURCE_URL}${path}`;
-
-  return fetch(endpoint, {
+  return fetch(`${PROXY_URL}${endpoint}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       Authorization: authString
     }
   })
+    .then(handleErrors)
     .then(response => response.json())
-    .catch(error => ({
-      statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`
-    }));
+    .catch(error => ({ error: error }));
 };
 
 /**
@@ -153,45 +176,61 @@ const getResource = (token, path) => {
  * @returns {object}
  */
 const getToken = code => {
-  return fetch(ACCESS_TOKEN_URL, {
+  return fetch(`${PROXY_URL}${ACCESS_TOKEN_URL}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     },
-    body: `grant_type=authorization_code&client_id=${API_KEY}&client_secret=${API_SECRET}&code=${code}&redirect_uri=${REDIRECT_URL}`
+    body:
+      `grant_type=authorization_code` +
+      `&client_id=${API_KEY}` +
+      `&client_secret=${API_SECRET}` +
+      `&code=${code}` +
+      `&redirect_uri=${REDIRECT_URL}`
   })
+    .then(handleErrors)
     .then(response => response.json())
-    .catch(error => ({
-      statusCode: 422,
-      body: `Oops! Something went wrong. ${error}`
-    }));
+    .catch(error => ({ error: error }));
 };
 
 /**
  * Init Function
+ * Uses authorization code URL parameter to request a token,
+ * then uses the token to request an image.
  */
 const init = async () => {
-  // Get an access token
+  // See if we received an authorization code as a URL parameter
+  if (!authCode) {
+    // Auth code does not exist. Put info in the html to send them on their way.
+    appElement.innerHTML = loggedOutMessage;
+  }
+
+  // Auth code exists. Now we need to get access token.
   let token = await getToken(authCode);
-  console.log("TOKEN:", token);
+  console.table(token);
 
-  // And send it!
+  // Display the logged out message if the token returns an error
+  if (token.error) {
+    appElement.innerHTML = loggedOutMessage;
+    return;
+  }
+
+  // Use the access token to request the image
   let image = await getResource(token, "/api/sharedfile/GA4");
-  console.log("IMAGE", image);
+  console.table(image);
 
-  // Output the array to page
-  appElement.innerHTML = `<pre>${JSON.stringify(image, undefined, 2)}</pre>`;
+  // Display an error if something goes wrong
+  if (image.error) {
+    appElement.innerHTML = image.error.message;
+    return;
+  }
+
+  // Output the image to page
+  appElement.innerHTML = `
+    <a href="${image.permalink_page}">
+      <img src="${image.original_image_url}" alt="${image.title}" />
+    </a>
+    <pre>${JSON.stringify(image, undefined, 2)}</pre>`;
 };
 
-// See if we received an authorization code as a URL parameter
-if (authCode) {
-  // Auth code exists. Now we need to get access token.
-  init();
-} else {
-  // Auth code does not exist. Put info in the html to send them on their way.
-  appElement.innerHTML = `
-    You need to authenticate this application with MLTSHP first.
-    <a href="${AUTHENTICATION_URL}">Go authenticate</a>.
-    (Be sure to click "I AGREE.")
-  `;
-}
+init();
